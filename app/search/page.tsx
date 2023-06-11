dc47a0ecc503ec59704176caf5c2b2dad774421a
Vasko Pavic <vaskopavic@gmail.com>
2023-06-11 19:24:42 +0200

    add song favoring functionality to the liked page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,15 @@
-import getSongsByTitle from "@/actions/getSongsByTitle";
-import Header from "@/components/Header";
-import SearchInput from "@/components/SearchInput";
 import SearchContent from "./components/SearchContent";
+import SearchInput from "@/components/SearchInput";
+import Header from "@/components/Header";
+import getSongsByTitle from "@/actions/getSongsByTitle";
 
 interface SearchProps {
   searchParams: {
     title: string;
   };
 }
+
+export const revalidate = 0;
 
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
